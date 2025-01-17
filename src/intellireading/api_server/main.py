e9b16ed66b1916317274d729f3ec36123965dc19
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2025-01-17 22:06:58 +0000

    chore: update pip installation commands across workflows and improve comments in entrypoint and Dockerfile

--- src/intellireading/api_server/main.py
+++ src/intellireading/api_server/main.py
@@ -1,8 +1,9 @@
 def entrypoint():
     import uvicorn
     from intellireading.api_server.app import app
+
     # this entrypoint should be used for dev purposes only
-    # for production, make sure you use a proper server 
+    # for production, make sure you use a proper server
     # and bind only to the appropriate interfaces
     uvicorn.run(app, host="0.0.0.0", port=80)  # noqa S104
 
