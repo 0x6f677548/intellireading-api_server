928f8d76b685d9674710cc5ba1a21864be5b8ae8
0x6f677548 <64972114+0x6f677548@users.noreply.github.com>
2025-01-18 18:59:36 +0000

    chore:code format

--- src/intellireading/api_server/routers/authentication.py
+++ src/intellireading/api_server/routers/authentication.py
@@ -35,22 +35,22 @@ class AuthConfig:
 
         def _mask_sensitive_values(config: dict) -> dict:
             import copy
+
             sensitive_keys = {"secret_key", "valid_api_keys"}
             masked_config = copy.deepcopy(config)
+
             def recursive_mask(d):
                 for k, v in d.items():
                     if isinstance(v, dict):
                         recursive_mask(v)
                     elif k.lower() in sensitive_keys:
                         d[k] = "****"
+
             recursive_mask(masked_config)
             return masked_config
 
-
-
         self._authentication_config = config.get("authentication", {}) if config else {}
-        _logger.info("Configuration for authentication: %s",
-                     _mask_sensitive_values(self._authentication_config))
+        _logger.info("Configuration for authentication: %s", _mask_sensitive_values(self._authentication_config))
 
         # turstile configuration
         _turnstile_config = self._authentication_config.get("turnstile", {})
